3af84fbbde01c3e5e58f148cec24decb386175d4
Maristane Figueiredo <maristanetecnologia@gmail.com>
2022-11-25 18:35:09 -0300

    feat:conexao cloud

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,38 +1,29 @@
 import { products } from "../database/db.js";
 
-
 export async function getProducts(req, res) {
-
   try {
     const { category, productName } = req.query;
 
-
     let productsData = [];
-    if (category) {  
-
+    if (category) {
       productsData = await products.find({ category }).toArray();
-    } else if (productName) {      
-
+    } else if (productName) {
       productsData = await products
         .find({ productName: { $regex: productName, $options: "i" } })
         .toArray();
-     
     } else {
-      
       productsData = await products.find().toArray();
     }
     return res.status(200).send(productsData);
-
   } catch (error) {
     console.error(error);
     return res.status(500).send({ message: error });
   }
 }
 
-
-
 export async function postProducts(req, res) {
   const body = req.body;
+  console.log("body", body);
 
   try {
     await products.insertOne(body);
