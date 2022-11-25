f23ac7a43c8209be36540f6ffdfdc1986a7c37c1
Maristane Figueiredo <maristanetecnologia@gmail.com>
2022-11-25 07:59:00 -0300

    reafact: productsRouter

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -9,17 +9,9 @@ import validationProductSchema from "../middlewares/productsSchemaMiddleware.js"
 
 const productsRouter = Router();
 
-// productsRouter.get("/products", async (request, response) => {
-//   return await products.controller.listarProdutos(request, response);
-// });
-
 productsRouter.get("/products", getProducts);
 
 productsRouter.post("/products", validationProductSchema, postProducts);
-
-// productsRouter.get("/categorys", async (request, response) => {
-//   return await products.controller.listarCategorias(request, response);
-// });
 
 productsRouter.get("/categorys", listCategory);
 
